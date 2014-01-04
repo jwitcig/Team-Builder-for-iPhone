5408cc27f228225e2c21b82e4c8a525468156358
jwitcig <jwitcig@gmail.com>
2014-01-04 14:30:22 -0600

    Initial Commit

---
+++ Team Builder for iPhone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Team Builder for iPhone
+//
+//  Created by Application Development on 1/4/14.
+//  Copyright (c) 2014 Jwit Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Team Builder for iPhone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Team Builder for iPhone
+//
+//  Created by Application Development on 1/4/14.
+//  Copyright (c) 2014 Jwit Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
